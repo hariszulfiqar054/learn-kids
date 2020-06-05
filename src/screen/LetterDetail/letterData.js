ec7afcb73caeb20a47a3609267df616e738d94c8
hariszulfiqar054 <hariszulfiqar054@gmail.com>
2020-06-06 00:59:43 +0500

    added images

---
+++ src/screen/LetterDetail/letterData.js
@@ -0,0 +1,126 @@
+export const letterData = () => [
+  {
+    word: "aeroplane",
+    img: require("../../../assets/letter/a.png"),
+  },
+  {
+    word: "bat",
+    img: require("../../../assets/letter/b.png"),
+  },
+  {
+    word: "moon",
+    img: require("../../../assets/letter/m.png"),
+  },
+  {
+    word: "cake",
+    img: require("../../../assets/letter/c.png"),
+  },
+  {
+    word: "corn",
+    img: require("../../../assets/letter/c.jpg"),
+  },
+  {
+    word: "eggplant",
+    img: require("../../../assets/letter/e.jpg"),
+  },
+  {
+    word: "horse",
+    img: require("../../../assets/letter/h.png"),
+  },
+  {
+    word: "ring",
+    img: require("../../../assets/letter/r.png"),
+  },
+  {
+    word: "home",
+    img: require("../../../assets/letter/h1.png"),
+  },
+  {
+    word: "clock",
+    img: require("../../../assets/letter/c1.png"),
+  },
+  {
+    word: "window",
+    img: require("../../../assets/letter/w.png"),
+  },
+  {
+    word: "tree",
+    img: require("../../../assets/letter/t.png"),
+  },
+  {
+    word: "leaf",
+    img: require("../../../assets/letter/l.png"),
+  },
+  {
+    word: "rabbit",
+    img: require("../../../assets/letter/r.jpg"),
+  },
+  {
+    word: "mountains",
+    img: require("../../../assets/letter/m1.png"),
+  },
+  {
+    word: "television",
+    img: require("../../../assets/letter/t1.png"),
+  },
+  {
+    word: "earth",
+    img: require("../../../assets/letter/e.png"),
+  },
+  {
+    word: "cloud",
+    img: require("../../../assets/letter/c2.png"),
+  },
+  {
+    word: "bee",
+    img: require("../../../assets/letter/b.jpg"),
+  },
+  {
+    word: "box",
+    img: require("../../../assets/letter/b1.png"),
+  },
+  {
+    word: "bear",
+    img: require("../../../assets/letter/b2.png"),
+  },
+  {
+    word: "panda",
+    img: require("../../../assets/letter/p.jpg"),
+  },
+  {
+    word: "hand",
+    img: require("../../../assets/letter/h2.png"),
+  },
+  {
+    word: "squirrel",
+    img: require("../../../assets/letter/s.png"),
+  },
+  {
+    word: "flower",
+    img: require("../../../assets/letter/f.png"),
+  },
+  {
+    word: "pen",
+    img: require("../../../assets/letter/p.png"),
+  },
+  {
+    word: "kite",
+    img: require("../../../assets/letter/k.png"),
+  },
+  {
+    word: "parrot",
+    img: require("../../../assets/letter/p1.jpg"),
+  },
+  {
+    word: "can",
+    img: require("../../../assets/letter/c3.png"),
+  },
+  {
+    word: "boy",
+    img: require("../../../assets/letter/b1.jpg"),
+  },
+  {
+    word: "family",
+    img: require("../../../assets/letter/f.jpg"),
+  },
+];
